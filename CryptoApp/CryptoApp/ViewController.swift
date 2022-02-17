9b7e188e62b5039f094d8c97542b71f690e68af7
ViktarKanaplia <vitto1989@icloud.com>
2022-02-17 20:02:45 +0300

    initial release

---
+++ CryptoApp/CryptoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoApp
+//
+//  Created by виктор конопля on 17.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
